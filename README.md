62fce3b4dc32cebc01a461519623d3c67518ab0c
Mustafa-Sayed-M <mustafaelsadiq9@gmail.com>
2023-11-19 14:39:22 +0200

    #

---
+++ README.md
@@ -0,0 +1,2 @@
+## Live Site
+<a href=""><img src="images/live-site.jpg" width="300" /></a>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+// 
+const annuallyPrice = document.querySelectorAll('.pricing-component .price.annually');
+const monthlyPrice = document.querySelectorAll('.pricing-component .price.monthly');
+
+console.log(annuallyPrice, monthlyPrice);
 // # Function to select plan
 function selectPlan(btn) {
     btn.classList.toggle('annually');
     btn.classList.toggle('monthly');
+    
+    if (btn.classList.contains('annually')) {
+        removeActiveClassFromAll(monthlyPrice);
+        addActiveClassFromAll(annuallyPrice);
+    } else {
+        removeActiveClassFromAll(annuallyPrice);
+        addActiveClassFromAll(monthlyPrice);
+    }
+}
+// # Function to remove active class from all items in array.
+function removeActiveClassFromAll(arr) {
+    arr.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+// # Function to add active class to all items in array.
+function addActiveClassFromAll(arr) {
+    arr.forEach(item => {
+        item.classList.add('active');
+    })
 }
